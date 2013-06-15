d516679049ee21e0ab14be3d88bdb80609a991e8
James Aitken <loonypandora@gmail.com>
2013-06-15 22:49:33 +0100

    Add a route for OS X testing

--- lib/JsonLinux.pm
+++ lib/JsonLinux.pm
@@ -1,13 +1,25 @@
 package JsonLinux;
-use Dancer ":syntax";
 
+use Dancer ":syntax";
 use common::sense;
+
 
 our $VERSION = "0.1.0";
 
+
+
+
 get "/" => sub {
     return [qw(hello foo bar)];
-
 };
+
+
+get "/cpu" => sub {
+    my $result = qx/sysctl -n machdep.cpu.brand_string/;
+    chomp($result);
+
+    return { cpu => $result };
+};
+
 
 true;
