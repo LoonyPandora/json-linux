4ddde3d571894d5aa24496f494f3c00da042d1af
James Aitken <loonypandora@gmail.com>
2013-06-16 04:01:35 +0100

    Fix up cpuinfo so it works

--- lib/JsonLinux.pm
+++ lib/JsonLinux.pm
@@ -7,18 +7,8 @@ use common::sense;
 our $VERSION = "0.1.0";
 
 
-
-
 get "/" => sub {
     return [qw(hello foo bar)];
-};
-
-
-get "/cpu" => sub {
-    my $result = qx/sysctl -n machdep.cpu.brand_string/;
-    chomp($result);
-
-    return { cpu => $result };
 };
 
 
--- lib/JsonLinux/API/cpuinfo.pm
+++ lib/JsonLinux/API/cpuinfo.pm
@@ -2,12 +2,32 @@ package JsonLinux::API::cpuinfo;
 
 use Dancer ':syntax';
 use Dancer::App;
+use Data::Dump qw(dump);
 
 use common::sense;
 
 
 get "/cpuinfo" => sub {
-    return [qw(cpuinfo)];
+    my @raw = qx{/bin/cat /proc/cpuinfo};
+
+    my @processor;
+    my $proc = 0;
+    for my $line (@raw) {
+        if ($line =~ /^\n$/) {
+            $proc++;
+        }
+
+        my ($key, $value) = split(":", $line);
+        chomp($key, $value);
+        $key   =~ s/\t+//g;
+        $value =~ s/^\s//g;
+
+        if ($key) {
+            $processor[$proc]{$key} = $value;
+        }
+    }
+
+    return \@processor;
 };
 
 
