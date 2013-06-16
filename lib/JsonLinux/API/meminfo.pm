a0ba54ac22926e7d514c811a525ad77b1d8172f4
James Aitken <loonypandora@gmail.com>
2013-06-16 04:11:30 +0100

    Add meminfo

--- lib/JsonLinux/API/meminfo.pm
+++ lib/JsonLinux/API/meminfo.pm
@@ -6,6 +6,22 @@ use Dancer::App;
 use common::sense;
 
 
+get "/meminfo" => sub {
+    my @raw = qx{/bin/cat /proc/meminfo};
+
+    my %output;
+    for my $line (@raw) {
+        my ($key, $value) = split(":", $line);
+        chomp($key, $value);
+        $key   =~ s/\t+//g;
+        $value =~ s/^\s+//g;
+
+        $output{$key} = $value
+
+    }
+
+    return \%output;
+};
 
 
 1;
